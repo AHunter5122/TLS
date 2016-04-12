890ec4a8fdf38ec9a6301d6f7bbff18e3add6ed5
ahunter16 <alex.hunter14@btinternet.com>
2016-04-12 20:42:19 +0100

    PRF, Premaster Secret, and keyexchange
    PRF has almost been completely implemented (still need to be able to
    specify length of output), pre-master secret can successfully be
    generated, and can now send ClientKeyExchange message
    also some general tidying of code and reordering of functions

--- TLS.cpp
+++ TLS.cpp
@@ -2,6 +2,7 @@
 #include <ESP8266WiFi.h>
 #include <string.h>
 #include <WiFiUdp.h>
+#include "sha256.h"
 
 TLS::TLS()
 {
@@ -9,23 +10,32 @@ TLS::TLS()
   return;
 }
 
-void TLS::connectWiFi(const char* id, const char* pass){
+bool TLS::connectWiFi(const char* id, const char* pass){
 
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(id);
-  
+  int timer = 0;
   WiFi.begin(id, pass);
   
   while (WiFi.status() != WL_CONNECTED) {
+    timer++;
     delay(500);
     Serial.print(".");
+    if(timer > 120){
+      WiFi.disconnect();
+      Serial.println();
+      Serial.println("WiFi timed out");
+      return false;
+    }
+
   }
   Serial.println("");
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  return true;
   
 }    
 
@@ -129,13 +139,6 @@ String TLS::hexValue(uint8_t ch){
   return buf;
 }
 
-int TLS::array_copy(uint8_t *main, int startaddr, uint8_t *extra, int extralen){
-  for(int i=startaddr; i <startaddr+extralen; i++){
-    *(main+i) = *(extra+i-startaddr);
-  }
-  return startaddr+extralen;
-
-}
 
 void TLS::client_hello(uint8_t* buffer){
   uint8_t content[] = "\x16";
@@ -185,28 +188,161 @@ void TLS::client_key_exchange(uint8_t *buffer, const char* id, int id_size){
   for(int i=0; i < id_size; i++){
     psk_id[i]=(uint8_t)id[i];
   }
+  for(int i =0; i < id_size; i++){
+    Serial.println(TLS::hexValue(psk_id[i]));
+  }
   uint8_t content[] = "\x16";
   uint8_t tls_version[] = "\x03\x03";
-  uint8_t key_exchange[] = "\x10";
-  uint8_t psk_id_size[] = "\x00\x00\x00";
   uint8_t outer[] = "\x00\x00";
-  uint16_t outerlength = (uint16_t)(sizeof(key_exchange)-1+id_size+sizeof(psk_id_size)-1);
-  uint32_t psklength = (uint32_t)(id_size);
+  uint8_t key_exchange[] = "\x10";
+  uint8_t psk_outer[] = "\x00\x00\x00";
+  uint8_t psk_inner[] = "\x00\x00";
+  uint16_t outerlength = (uint16_t)(sizeof(key_exchange)-1+id_size+sizeof(psk_outer)-1+sizeof(psk_inner)-1);
+  uint32_t psk_outer_length = (uint32_t)((int)id_size+2);
+  uint16_t psk_inner_length = (uint16_t)(id_size);
 
   outer[0] = (uint8_t)(outerlength & 0xff00);
   outer[1] = (uint8_t)(outerlength & 0xff);
-  psk_id_size[0] = (uint8_t)(psklength & 0xff0000);
-  psk_id_size[1] = (uint8_t)(psklength & 0xff00);
-  psk_id_size[2] = (uint8_t)(psklength & 0xff);
+  psk_outer[0] = (uint8_t)(psk_outer_length & 0xff0000);
+  psk_outer[1] = (uint8_t)(psk_outer_length & 0xff00);
+  psk_outer[2] = (uint8_t)(psk_outer_length & 0xff);
+  psk_inner[0] = (uint8_t)(psk_inner_length & 0xff00);
+  psk_inner[1] = (uint8_t)(psk_inner_length & 0xff);
 
   int index = 0;
   index = TLS::array_copy(buffer, index, content, (int)(sizeof(content)-1));
   index = TLS::array_copy(buffer, index, tls_version, (int)(sizeof(tls_version)-1));
   index = TLS::array_copy(buffer, index, outer, (int)(sizeof(outer)-1));
   index = TLS::array_copy(buffer, index, key_exchange, (int)(sizeof(key_exchange)-1));
-  index = TLS::array_copy(buffer, index, psk_id_size, (int)(sizeof(psk_id_size)-1));
-  index = TLS::array_copy(buffer, index, psk_id, (int)(sizeof(psk_id)-1));
+  index = TLS::array_copy(buffer, index, psk_outer, (int)(sizeof(psk_outer)-1));
+  index = TLS::array_copy(buffer, index, psk_inner, (int)(sizeof(psk_inner)-1));
+  index = TLS::array_copy(buffer, index, psk_id, id_size);
+}
+void TLS::change_cipher_spec(uint8_t* buffer, int bufferSize){
+  uint8_t content[] = "\x14\x03\x03\x00\x01\x01";
+  //TLS::check_memory(buffer, 6);
+  int index = 0;
+  index = TLS::array_copy(buffer, index, content, (int)(sizeof(content)-1));
+
+}
+
+bool TLS::check_memory(uint8_t** buffer, int bufferSize){
+  if(sizeof(buffer) <= bufferSize+1){
+    uint8_t* test = NULL;
+    test = (uint8_t*) realloc(*buffer, bufferSize);
+    if(test!=NULL){
+      *buffer = test;
+      Serial.println("reallocated");
+      
+    }
+    else{
+      free(buffer);
+      Serial.println("Error reallocating memory");
+      return false;
+    }
+  }
+  return true;
+  
+}
+
+int TLS::array_copy(uint8_t *main, int startaddr, uint8_t *extra, int extralen){
+  for(int i=startaddr; i <startaddr+extralen; i++){
+    *(main+i) = *(extra+(i-startaddr));
+  }
+  return startaddr+extralen;
+
+}
+void TLS::hmac256(uint8_t* secret, int secretSize, uint8_t* data, int dataSize, uint8_t* temp){
+  // double ms;
+  delay(1000);
+  //ms = millis();
+  Sha256.initHmac(secret,secretSize);
+
+  for (int i=0; i < dataSize; i++){
+    Sha256.write(data[i]);
+  }
+
+  uint8_t* result = Sha256.resultHmac();
+  printHash(result);
+  Serial.println("done");
+  // Serial.print("Hash took ");
+  // Serial.print((millis() - ms));
+  // Serial.println(" ms");
+  memcpy(temp, result, 32);
+  Serial.println();
+  
 }
 
 
-//uint8_t packet[] = "\x16\x03\x03\x00\xf0\x01\x00\x00\xec\x03\x03\x4e\x24\x3b\x32\xaf\x37\xd2\x95\xee\x3c\x95\xc2\xf1\xb9\x21\xae\x3f\x6f\xbe\x37\x64\xa7\xcb\x6f\xb8\x00\xe9\x4e\x52\x40\x46\xae\x00\x00\x02\x00\x8c\x01\x00\x00\xc2\x00\x00\x00\x00\x00\x23\x00\x00\x00\x0d\x00\x04\x00\x02\x04\x00\x00\x0f\x00\x01\x01\x00\x15\x00\xa8\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+void TLS::PRF(uint8_t* secret, char* label, uint8_t* seed, int  secretSize, int labelSize, int seedSize, int iterations){
+  uint8_t* data = (uint8_t* )malloc((labelSize+seedSize));
+  uint8_t* temp = (uint8_t* )malloc(32);
+  uint8_t* fulldata = (uint8_t* )malloc((32+labelSize+seedSize));
+  uint8_t* cache = (uint8_t* )malloc(96);
+
+  int addr = 0;
+
+  for(int i =0; i < labelSize; i++){
+    data[i] = (uint8_t)label[i];
+  }
+
+  TLS::array_copy(data, labelSize, seed, seedSize);
+
+  TLS::hmac256(secret, secretSize, data, (seedSize+labelSize), temp);
+  addr = TLS::array_copy(cache, addr, temp, 32);
+  
+  for(int i = 0; i < iterations; i++){
+    TLS::array_copy(fulldata, 0, temp, (32+labelSize+seedSize));
+    TLS::array_copy(fulldata, 32, data, (labelSize+seedSize));
+    
+    TLS::hmac256(secret, secretSize, fulldata, 32+labelSize+seedSize, temp);
+    
+    addr = TLS::array_copy(cache, addr, temp, 32);
+  }
+  Serial.println();
+  Serial.println("cache:");
+  for(int i =0; i < 96; i ++){
+    Serial.print(TLS::hexValue(cache[i]));
+  }
+  Serial.println();
+  free(temp);
+  free(fulldata);
+  free(cache);
+  free(data);
+}
+
+void TLS::printHash(uint8_t* hash) {
+  int i;
+  for (i=0; i<32; i++) {
+    Serial.print(TLS::hexValue(hash[i]));
+  }
+  Serial.println();
+
+}
+
+uint8_t* TLS::preMasterSecret(const char* psk, int pskSize){
+  uint8_t* pms = (uint8_t* )malloc(4+pskSize+pskSize);
+  uint8_t outer[2];
+  uint16_t length = (uint16_t)pskSize;
+  int zero = 0;
+  outer[0] = (uint8_t)(length & 0xff00);
+  outer[1] = (uint8_t)(length & 0xff);
+
+  TLS::array_copy(pms, 0, outer, 2);
+  for(int i = 2; i < pskSize+2; i++){
+    pms[i] = (uint8_t)zero;
+  }
+
+  TLS::array_copy(pms, 2+pskSize, outer, 2);
+
+  for(int i = 0; i < pskSize; i++){
+    *(pms+(i+pskSize+4)) = (uint8_t)*(psk+i);
+  }
+
+  Serial.println("pre-master secret:");
+  for (int i = 0; i < 4+pskSize+pskSize; i++){
+    Serial.print(TLS::hexValue(pms[i]));
+  }
+  Serial.println("end secret");
+  return pms;
+}
--- TLS.h
+++ TLS.h
@@ -12,13 +12,19 @@ public:
 	TLS();
 	static void parse(uint8_t* searchstr, WiFiClient client, uint8_t* result, int parseLength, int padding);
 	static int intConvert(uint8_t *result, int parseLength);
-	static void connectWiFi(const char* id, const char* pass);
+	static bool connectWiFi(const char* id, const char* pass);
 	static String hexValue(uint8_t ch);
 	static bool charScan(const uint8_t* value, WiFiClient client);
 	static unsigned long sendNTPpacket(IPAddress& address, WiFiUDP udp, const int NTP_PACKET_SIZE);
 	static void client_hello(uint8_t* buffer);
 	static void client_key_exchange(uint8_t *buffer, const char* id, int id_size);
+	static void change_cipher_spec(uint8_t* buffer, int bufferSize);
 	static int array_copy(uint8_t *main, int startaddr, uint8_t *extra, int extralen);
+	static bool check_memory(uint8_t** buffer, int bufferSize);
+	static void hmac256(uint8_t* secret, int secretSize, uint8_t* data, int dataSize, uint8_t* temp);
+	static void printHash(uint8_t* hash);
+	static void PRF(uint8_t* secret, char* label, uint8_t* seed, int  secretSize, int labelSize, int seedSize, int iterations);
+	static uint8_t* preMasterSecret(const char* psk, int psk_size);
 	static byte packetBuffer[];
 
 };
